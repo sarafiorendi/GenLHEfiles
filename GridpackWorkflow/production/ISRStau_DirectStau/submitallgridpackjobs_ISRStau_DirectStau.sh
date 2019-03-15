92d7fa13164a5d4e7399e71f22c30a0dfe54953e
dspitzba <daniel.spitzbart@cern.ch>
2019-03-15 09:43:26 +0100

    fix proc card of ISRStau_EWKino

--- GridpackWorkflow/production/ISRStau_DirectStau/submitallgridpackjobs_ISRStau_DirectStau.sh
+++ GridpackWorkflow/production/ISRStau_DirectStau/submitallgridpackjobs_ISRStau_DirectStau.sh
@@ -8,20 +8,20 @@ PARTN1="_mN1-"
 
 ### Create cards and SLHAs for all mass points
 
-#for MNLSP in 100 150 200 250 300 350 400; do
+for MNLSP in 100 150 200 250 300 350 400; do
 #for MNLSP in 100; do
-for MNLSP in 400; do
-  #for DM in 10 20 30 40 50; do
-  for DM in 40; do
-    if [ $MNLSP -eq 100 -a $DM -eq 10 ]
-    then
-      continue
-    else
+#for MNLSP in 400; do
+  for DM in 10 20 30 40 50; do
+  #for DM in 40; do
+    #if [ $MNLSP -eq 100 -a $DM -eq 10 ]
+    #then
+    #  continue
+    #else
       MN1=`awk "BEGIN {printf \"%.2f\n\", (${MNLSP}-${DM})}"`
       MN1STR=${MN1/./p}
       MSTSTR=${MNLSP/./p}
       python ${SCRIPT} ${MODEL}${MSTSTR}${PARTN1}${MN1STR} --cards-dir ${JOBS}/${MODEL}${MSTSTR}${PARTN1}${MN1STR}  --genproductions-dir ${genprodir}
-    fi
+    #fi
   done
 done
 
