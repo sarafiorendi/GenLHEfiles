ecad55ea0c703ebf057771068f9b3ea2067fbb01
scodella <luca.scodellaro@cern.ch>
2019-07-25 17:08:06 +0200

    script to clone FastSim requests

--- GridpackWorkflow/production/models/clone_Summer16FStoFall17FS_requests.py
+++ GridpackWorkflow/production/models/clone_Summer16FStoFall17FS_requests.py
@@ -8,11 +8,11 @@ from json import dumps
 mcm = McM(dev=False)
 
 #list of requests to clone 
-#requests_to_clone = [ 'SUS-RunIIFall17FSPremix-00120',
-#                      'SUS-RunIIFall17FSPremix-00121', 
+#requests_to_clone = [ 'SUS-RunIISummer16FSPremix-00143', 
+#                      'SUS-RunIISummer16FSPremix-00144',
 #                       ] 
 requests_to_clone = [ ]
-ist0 = list('SUS-RunIIFall17FSPremix-00' + str(i) for i in range(156,160+1))
+list0 = list('SUS-RunIISummer16FSPremix-00' + str(i) for i in range(179,183+1))
 for req in list0:
     requests_to_clone.append(req)
 
@@ -20,7 +20,7 @@ for req in list0:
 f = open('new_requests.txt', 'w')
 
 # If member_of_campaign is different, it will clone to other campaign
-campaign_modifications = {'member_of_campaign': 'RunIIAutumn18FSPremix'}
+campaign_modifications = {'member_of_campaign': 'RunIIFall17FSPremix'}
 
 for request_prepid_to_clone in requests_to_clone:
     
@@ -48,12 +48,21 @@ for request_prepid_to_clone in requests_to_clone:
     cloned_request = mcm.get('requests', clone_answer['prepid'] )
 
     total_events = request['total_events']
-    modifications = {'extension': 0,
-                     'total_events': int(total_events*1.5),
+    datasetname  = request['dataset_name'].replace('TuneCUETP8M1', 'TuneCP2')
+    fragment     = request['fragment']
+    fragment     = fragment.replace('from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *', 
+                                    'from Configuration.Generator.MCTunes2017.PythiaCP2Settings_cfi import *')
+    fragment     = fragment.replace('pythia8CUEP8M1SettingsBlock', 'pythia8CP2SettingsBlock')
+    fragment     = fragment.replace('pythia8CUEP8M1Settings',      'pythia8CP2Settings')
+
+    modifications = {'dataset_name'   : datasetname,
+                     'fragment'       : fragment,
+                     'extension'      : 0,
+                     'total_events'   : int(total_events*1.1),
                      'process_string' : '',
-                     #'memory' : 4000,
-                     'keep_output' : [False] 
-                     #'keep_output' : [True] # Special for long-lived models
+                     #'memory'         : 4000,
+                     'keep_output'    : [False] 
+                     #'keep_output'    : [True] # Special for long-lived models
                      }
 
     # Make predefined modifications
