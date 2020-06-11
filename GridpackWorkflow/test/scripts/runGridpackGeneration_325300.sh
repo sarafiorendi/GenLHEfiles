3851e0813940077b82a12a28a7f22641e37f9f3d
scodella <luca.scodellaro@cern.ch>
2020-06-11 18:36:29 +0200

    script test UL2019

---
+++ GridpackWorkflow/test/scripts/runGridpackGeneration_325300.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+### Script to run gridpack generation via a condor job
+### Intended for use with Condor at UCSD
+
+### Author: Dustin Anderson
+
+PROCESS=$1
+OUTDIR=$2
+
+echo "Process"
+echo $PROCESS
+
+WORKDIR=$(pwd)
+echo "Working directory: "
+echo $WORKDIR
+
+echo "Output directory set to: "
+echo $OUTDIR
+
+#copy patches and cards into correct location
+mkdir patches
+cp ${WORKDIR}/ucsd.patch patches/
+#cp ${WORKDIR}/mgfixes.patch patches/
+#cp ${WORKDIR}/models.patch patches/
+mkdir -p cards/${PROCESS}
+cp ${WORKDIR}/${PROCESS}_run_card.dat cards/${PROCESS}
+cp ${WORKDIR}/${PROCESS}_proc_card.dat cards/${PROCESS}
+cp ${WORKDIR}/${PROCESS}_customizecards.dat cards/${PROCESS}
+cp ${WORKDIR}/${PROCESS}_param_card.dat cards/${PROCESS}
+
+#run script
+echo "Running gridpack generation"
+./gridpack_generation_325300.sh ${PROCESS} cards/${PROCESS} 
+
+#copy output
+echo "Copy output"
+ls ${PROCESS}_*_tarball.tar.xz
+mv ${PROCESS}_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz ${PROCESS}_tarball.tar.xz
+#lcg-cp -v -b -D srmv2 --vo cms file:${PROCESS}_tarball.tar.xz srm://bsrm-3.t2.ucsd.edu:8443/srm/v2/server?SFN=${OUTDIR}/${PROCESS}_tarball.tar.xz
+#gfal-copy -p -f -t 4200 ${PROCESS}_tarball.tar.xz gsiftp://gftp.t2.ucsd.edu/${OUTDIR}/${PROCESS}_tarball.tar.xz --checksum ADLER32
+gfal-copy -p -f -t 4200 ${PROCESS}_tarball.tar.xz ${OUTDIR}/${PROCESS}_tarball.tar.xz --checksum ADLER32
+#lcg-cp -v -b -D srmv2 --vo cms file:${PROCESS}.log srm://bsrm-3.t2.ucsd.edu:8443/srm/v2/server?SFN=${OUTDIR}/${PROCESS}.log
+#gfal-copy -p -f -t 4200 ${PROCESS}.log gsiftp://gftp.t2.ucsd.edu/${OUTDIR}/${PROCESS}.log --checksum ADLER32
+gfal-copy -p -f -t 4200 ${PROCESS}.log ${OUTDIR}/${PROCESS}.log --checksum ADLER32
+
+echo "ls in production dir"
+ls
+
+#cleanup
+cd $WORKDIR
+echo "ls in workdir"
+ls
+rm -rf genproductions
+
+echo "Bye."
