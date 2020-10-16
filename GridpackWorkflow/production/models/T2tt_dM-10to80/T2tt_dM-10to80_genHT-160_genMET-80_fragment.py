e89e30edbd55773132ac0d41bb8ac44b3e7f2942
Andrea Trapote <andreatrapote21@gmail.com>
2020-10-16 11:57:59 +0200

    Updating T2tt and T2bW fragments with dM 6to8

--- GridpackWorkflow/production/models/T2tt_dM-10to80/T2tt_dM-10to80_genHT-160_genMET-80_fragment.py
+++ GridpackWorkflow/production/models/T2tt_dM-10to80/T2tt_dM-10to80_genHT-160_genMET-80_fragment.py
@@ -35,7 +35,6 @@ BLOCK MASS  # Mass Spectrum
    1000035     1.00000000E+05   # ~chi_40
    1000024     1.00000000E+05   # ~chi_1+
    1000037     1.00000000E+05   # ~chi_2+
-
 # DECAY TABLE
 #         PDG            Width
 DECAY   1000001     0.00000000E+00   # sdown_L decays
@@ -52,7 +51,6 @@ DECAY   1000006     1.00000000E+00   # stop1 decays
     0.00000000E+00    4    1000022      5     -1    2  # dummy allowed decay, in order to turn on off-shell decays
     1.00000000E+00    3    1000022      5   24
 DECAY   2000006     0.00000000E+00   # stop2 decays
-
 DECAY   1000011     0.00000000E+00   # selectron_L decays
 DECAY   2000011     0.00000000E+00   # selectron_R decays
 DECAY   1000012     0.00000000E+00   # snu_elL decays
@@ -79,9 +77,28 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
     comEnergy = cms.double(13000.),
     RandomizedParameters = cms.VPSet(),
 )
+
+# Parameters that define the grid in the bulk and diagonal
+class gridBlock:
+  def __init__(self, xmin, xmax, xstep, ystep):
+    self.xmin = xmin
+    self.xmax = xmax
+    self.xstep = xstep
+    self.ystep = ystep
     
 model = "T2tt_dM-10to80_genHT-160_genMET-80"
 process = "StopStop"
+
+# Number of events: min(goalLumi*xsec, maxEvents) (always in thousands)
+goalLumi = 400
+minLumi = 50
+minEvents, maxEvents = 40, 1000
+xdiagStep, ydiagStep = 25, 10
+minDM, maxDM = 10, 80
+
+scanBlocks = []
+scanBlocks.append(gridBlock(250,  801, 25, 10))
+ymin, ymax = 0, 1100
 
 def matchParams(mass):
   if mass>99 and mass<199: return 62., 0.498
@@ -96,18 +113,28 @@ def xsec(mass):
   if mass < 300: return 319925471928717.38*math.pow(mass, -4.10396285974583*math.exp(mass*0.0001317804474363))
   else: return 6953884830281245*math.pow(mass, -4.7171617288678069*math.exp(mass*6.1752771466190749e-05))
 
+# Number of events for mass point, in thousands
+def events(mass):
+  xs = xsec(mass)
+  nev = min(goalLumi*xs, maxEvents*1000)
+  if nev < xs*minLumi: nev = xs*minLumi
+  nev = max(nev/1000, minEvents)
+  return math.ceil(nev) # Rounds up
+
 # -------------------------------
 #    Constructing grid
 mpoints = []
-nev=250
-def GetAllStopNeutralinoPoints(minStop = 300, maxStop = 1000, dStop = 100, mindif = 6, maxdif = 8, ddif = 2):
-  points = []
-  for mStop in range(minStop, maxStop+dStop, dStop):
-    for dif in range(mindif, maxdif+ddif, ddif):
-      mChi = mStop-dif
-      points.append([mStop, mChi,nev])
-  return points
-mpoints=GetAllStopNeutralinoPoints()
+Ndiag = 0
+xmin, xmax = 9999, 0
+for block in scanBlocks:
+  for mx in range(block.xmin, block.xmax, xdiagStep):
+    xmin = min(block.xmin, xmin)
+    xmax = min(block.xmin, xmax)
+    for my in range(mx-maxDM, mx-minDM+1, ydiagStep):
+      if my > ymax: continue
+      nev = events(mx)#*eff[mx][my]
+      Ndiag += nev
+      mpoints.append([mx,my, nev])
 
 for point in mpoints:
     mstop, mlsp = point[0], point[1]
@@ -258,4 +285,3 @@ ProductionFilterSequence = cms.Sequence(generator*
                                         tmpAk4GenJetsNoNu * genHTFilter * 
                                         tmpGenMetTrue * genMETfilter1 * genMETfilter2
 )
-
--- GridpackWorkflow/production/models/T2tt_dM-10to80/T2tt_dM-10to80_genHT-160_genMET-80_grid.py
+++ GridpackWorkflow/production/models/T2tt_dM-10to80/T2tt_dM-10to80_genHT-160_genMET-80_grid.py
@@ -10,39 +10,74 @@ import os,sys,math
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from grid_utils import *
 
-model = "T2tt_dM-10to80_genHT-160_genMET-80"
+# Parameters that define the grid in the bulk and diagonal
+class gridBlock:
+  def __init__(self, xmin, xmax, xstep, ystep):
+    self.xmin = xmin
+    self.xmax = xmax
+    self.xstep = xstep
+    self.ystep = ystep
+    
+model = "T2tt_4bd"
 process = "StopStop"
 
 period = "Summer16"
+
+# Number of events: min(goalLumi*xsec, maxEvents) (always in thousands)
+goalLumi = 400
+if "16" in period : minLumi = 50
+elif "17" in priod : minLumi = 57 
+minEvents, maxEvents = 40, 1000
+xdiagStep, ydiagStep = 25, 10
+minDM, maxDM = 10, 80
+
+scanBlocks = []
+if period == "Spring16" :
+  scanBlocks.append(gridBlock(250,  801, 25, 10)) #Using only [x,y]diagStep
+  ymin, ymax = 0, 1100 
+elif (period == "Summer16" or period == "Fall17") :
+  scanBlocks.append(gridBlock(250, 1101, 25, 10)) #Using only [x,y]diagStep
+  ymin, ymax = 0, 1400
+
+# Number of events for mass point, in thousands
+def events(mass):
+  xs = xsec(mass,process)
+  nev = min(goalLumi*xs, maxEvents*1000)
+  if nev < xs*minLumi: nev = xs*minLumi
+  nev = max(nev/1000, minEvents)
+  return math.ceil(nev) # Rounds up
 
 # -------------------------------
 #    Constructing grid
 
 mpoints = []
-nev=250
-def GetAllStopNeutralinoPoints(minStop = 300, maxStop = 1000, dStop = 100, mindif = 6, maxdif = 8, ddif = 2):
-  points = []
-  for mStop in range(minStop, maxStop+dStop, dStop):
-    for dif in range(mindif, maxdif+ddif, ddif):
-      mChi = mStop-dif
-      points.append([mStop, mChi,nev])
-  return points
-mpoints=GetAllStopNeutralinoPoints()
+Ndiag = 0
+xmin, xmax = 9999, 0
+for block in scanBlocks:
+  for mx in range(block.xmin, block.xmax, xdiagStep):
+    xmin = min(block.xmin, xmin)
+    xmax = min(block.xmin, xmax)
+    for my in range(mx-maxDM, mx-minDM+1, ydiagStep):
+      if my > ymax: continue
+      nev = events(mx)
+      Ndiag += nev
+      mpoints.append([mx,my, nev])
 
 ## Test print out for repeated points
 mset = set()
 for mp in mpoints: mset.add(mp[0]*10000+mp[1])
 Ntot, Ndiff = len(mpoints), len(mset)
-if Ntot==Ndiff: print "\nGrid contains "+str(Ntot)+" mass points. No duplicates\n"
-else: print "\n\nGRID CONTAINS "+str(Ntot-Ndiff)+" DUPLICATE POINTS!!\n\n"
+nev_s = "{0:.1f}".format(Ndiag/1000)
+if Ntot==Ndiff: print "\nGrid contains "+str(Ntot)+" mass points with "+nev_s+"M events. No duplicates\n"
+else: print "\n\nGRID CONTAINS "+str(Ntot-Ndiff)+" DUPLICATE MASS POINTS!!\n\n"
 
 # -------------------------------
 #     Plotting and printing
-xmin, xmax, xstep = 300, 1000, 100
-ymin, ymax, ystep = 292, 994, 100
 
 makePlot([mpoints], 'events', model, process, xmin, xmax, ymin, ymax)
+Ntot = makePlot([mpoints], 'lumi', model, process, xmin, xmax, ymin, ymax)
+Ntot = makePlot([mpoints], 'lumi_br4', model, process, xmin, xmax, ymin, ymax)
+
 
 print 'Average matching efficiency (for McM and GEN fragment) = '+"{0:.3f}".format(getAveEff(mpoints,process))
 print
-
