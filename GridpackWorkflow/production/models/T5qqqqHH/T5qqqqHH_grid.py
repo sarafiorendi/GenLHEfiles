408a4b156454e40a8eff89d40982352884fac10c
Carlos Francisco Erice Cid <carlos.francisco.erice.cid@cern.ch>
2021-05-21 19:10:15 +0200

    Unfiltered T5qqqqHH

---
+++ GridpackWorkflow/production/models/T5qqqqHH/T5qqqqHH_grid.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+### Script to define scan grid
+
+### Authors:
+### Manuel Franco Sevilla
+### Ana Ovcharova
+
+import os,sys,math
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from grid_utils import *
+
+# Parameters that define the grid in the bulk and diagonal
+class gridBlock:
+  def __init__(self, xmin, xmax, xstep):
+    self.xmin = xmin
+    self.xmax = xmax
+    self.xstep = xstep
+
+
+model   = "T5qqqqHH"
+process = "GlGl"    
+scanBlocksMGlu = []
+scanBlocksMGlu.append(gridBlock(1000, 2551, 50))
+scanBlocksMN1 = []
+scanBlocksMN1.append(gridBlock(0, 1601, 50))
+dMGluN1min = 200
+dMGluN2    = 50
+# weighted average of matching efficiencies for the full scan
+# must equal the number entered in McM generator params
+nev = 50
+
+# -------------------------------
+#    Constructing grid
+cols = []
+Nevents = []
+xmin, xmax = 9999, 0
+ymin, ymax = 9999, 0
+for block in scanBlocksMGlu:
+  Nbulk, Ndiag = 0, 0
+  for mx in range(block.xmin, block.xmax, block.xstep):
+    xmin = min(xmin, block.xmin)
+    xmax = max(xmax, block.xmax)
+    col = []
+    for blockLSP in scanBlocksMN1:
+      for my in range(blockLSP.xmin, blockLSP.xmax, blockLSP.xstep):
+        if (mx - my) < dMGluN1min: continue
+        ymin = min(ymin, my)
+        ymax = max(ymax, my)
+        col.append([mx,my,nev])
+        Nbulk += nev
+    cols.append(col)
+  Nevents.append([Nbulk, Ndiag])
+mpoints = []
+for col in cols: mpoints.extend(col)
+## Test print out for repeated points
+mset = set()
+for mp in mpoints: mset.add(mp[0]+mp[1]*10000)
+Ntot, Ndiff = len(mpoints), len(mset)
+if Ntot==Ndiff: print "\nGrid contains "+str(Ntot)+" mass points. No duplicates\n"
+else: print "\n\nGRID CONTAINS "+str(Ntot-Ndiff)+" DUPLICATE POINTS!!\n\n"
+
+# -------------------------------
+#     Plotting and printing
+
+makePlot(cols, 'events', model, process, xmin, xmax, ymin, ymax)
+Ntot = makePlot(cols, 'lumi', model, process, xmin, xmax, ymin, ymax)
+#makePlot(cols, 'factor')
+
+print '\nScan contains '+"{0:,.0f}".format(Ntot)+" events\n"
+print 'Average matching efficiency (for McM and GEN fragment) = '+"{0:.3f}".format(getAveEff(mpoints,process))
+print
+
+print
