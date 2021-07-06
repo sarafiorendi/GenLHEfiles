da5cc8fda677ec768b16086cd7e58f5ca2c3f5a5
Diogo de Bastos <diogodebastos18@gmail.com>
2021-07-06 15:06:51 +0200

    little bit of script cleaning and commenting

--- GridpackWorkflow/test/scripts/plotFilterEff.py
+++ GridpackWorkflow/test/scripts/plotFilterEff.py
@@ -7,32 +7,44 @@ from DataFormats.FWLite import Events, Handle, Lumis
 import glob
 from collections import OrderedDict
 
-#dasgoclient -query="file dataset=/SMS-T2tt_dM-10to80_genHT-160_genMET-80_mWMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PUFall17Fast_94X_mc2017_realistic_v15-v1/MINIAODSIM" > T2tt_2017.txt
+
+# To measure the filter eff you can use local samples or access files remotely via cms-xrd-global.cern.ch/xrootd-cms.infn.it
+# for remote access start your voms proxy
 
 #fileBase = "/hadoop/cms/store/user/scasasso/mcProduction/AODSIM/T2bW_X05_dM-10to80/T2bW_X05_dM-10to80_genHT-160_genMET-80_{0}.root"
-fileBase = "root://cms-xrd-global.cern.ch///store/mc/RunIIAutumn18MiniAOD/SMS-T2tt_dM-10to80_genHT-160_genMET-80_mWMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUFall18Fast_102X_upgrade2018_realistic_v15-v1/260000/6D0540CE-2D04-3B42-A26B-E8777B3C0924.root"
-fileBase = "root://cms-xrd-global.cern.ch//store/mc/RunIIFall17MiniAODv2/SMS-T2tt_dM-10to80_genHT-160_genMET-80_mWMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUFall17Fast_94X_mc2017_realistic_v15-v1/40000/C01C2B31-CF20-E911-A7A7-48D539D33333.root" 
-#fileBase = "root://xrootd-cms.infn.it//store/mc/RunIIFall17MiniAODv2/SMS-T2tt_dM-10to80_genHT-160_genMET-80_mWMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUFall17Fast_94X_mc2017_realistic_v15-v1/10000/005AD2C8-0D21-E911-9D61-0025905C53F0.root"
+#fileBase = "root://cms-xrd-global.cern.ch///store/mc/RunIIAutumn18MiniAOD/SMS-T2tt_dM-10to80_genHT-160_genMET-80_mWMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUFall18Fast_102X_upgrade2018_realistic_v15-v1/260000/6D0540CE-2D04-3B42-A26B-E8777B3C0924.root"
 
-YEAR="2017"
+# Another option, without processing samples is to access the full dataset via dasgoclient
+# Let's follow the example for the signal samples SMS-T2tt_dM-10to80_genHT-160_genMET-80_mWMin-0p1
+# First, query dasgoclient interactively to get the location of all the sample files and save it
+#dasgoclient -query="file dataset=/SMS-T2tt_dM-10to80_genHT-160_genMET-80_mWMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-PUFall18Fast_102X_upgrade2018_realistic_v15-v1/MINIAODSIM" > T2tt_2018.txt
+
+# Name of the output root file
 YEAR="2018"
 hfilename='filterEffs_SMS_T2tt_dM_10to80_genHT_160_genMET_80_mWMin_0p1_'+YEAR+'.root'
 
 def main():
-
     fileList = []
-    samplesFile = open('T2tt_'+YEAR+'.txt')
+    # .txt filename where you saved the samples files from dasgoclient
+    samplesFileName = "T2tt_"+YEAR+".txt"
+    number_of_lines = len(open(samplesFileName).readlines())
+    samplesFile = open(samplesFileName)
     samples = samplesFile.readlines()
     for sample in samples:
-      fileList += ["root://cms-xrd-global.cern.ch/"+sample.replace('\n', '')]
+      # Change servers in case the script fails in a specific file
+      #fileList += ["root://cms-xrd-global.cern.ch/"+sample.replace('\n', '')]
+      fileList += ["root://xrootd-cms.infn.it/"+sample.replace('\n', '')]
     #print fileList
 
-    #fileList = [fileBase] #fileList = [fileBase.format(i) for i in range(1,101)]
+    #fileList = [fileBase.format(i) for i in range(1,101)] # for samples saved locally
+    #fileList = [fileBase] # in case you're using only 1 file remotely, good to debug
 
     modelDict = {}
+    prog=0.0
 
     for file in fileList:
         print '  Inspecting file {0}'.format(file)
+        print "Progress: %.2f" % (prog/number_of_lines*100) + " %"
 
         lumis = Lumis(file)
         events = Events(file)
@@ -58,6 +70,7 @@ def main():
             else:
                 modelDict[modelShort]["pass"] += float(genFilter.numEventsPassed())
                 modelDict[modelShort]["total"] += float(genFilter.numEventsTotal())
+        prog += 1
       
     oModelDict = OrderedDict(sorted(modelDict.items()))
 
@@ -80,14 +93,14 @@ def main():
     c = r.TCanvas("c","c")
     c.cd()
     hFilterEff.Draw()
-    c.SaveAs("FilterEff.pdf")
-    c.SaveAs("FilterEff.png")
+    c.SaveAs("FilterEff_"+YEAR+".pdf")
+    c.SaveAs("FilterEff_"+YEAR+".png")
 
     hfile = r.TFile(hfilename, 'RECREATE')
 
-    #hfile.cd()
+    # Save the effs hist in root format
+    # You can later access the effs by: hFilterEff->GetBinContent(hFilterEff->GetXaxis()->FindBin("375_315"))
     hFilterEff.Write()
-    #hfile.Close()
 
     return True
     
